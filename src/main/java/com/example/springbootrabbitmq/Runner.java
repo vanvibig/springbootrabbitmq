a8cf901ca372fa030cc606bde9e4755a3f2b7d8c
nvvi <vanvibig@gmail.com>
2019-06-13 18:17:50 +0700

    fin but not run

---
+++ src/main/java/com/example/springbootrabbitmq/Runner.java
@@ -0,0 +1,33 @@
+package com.example.springbootrabbitmq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+
+    public Runner(RabbitTemplate rabbitTemplate, Receiver receiver) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.receiver = receiver;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Senđing message...");
+        rabbitTemplate.convertAndSend(
+                SpringbootrabbitmqApplication.topicExchangeName,
+                "foo.bar.baz",
+                "Hello from RabbitMQ!");
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+}
